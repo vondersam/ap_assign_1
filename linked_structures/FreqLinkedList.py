0a3c9365b2b200741170720f039e4934770f182b
Samuel Rodriguez <samuelyret@gmail.com>
2018-02-01 22:51:29 +0100

    return list in alphabetical order

--- linked_structures/FreqLinkedList.py
+++ linked_structures/FreqLinkedList.py
@@ -26,7 +26,7 @@ class Node:
 
 class FreqLinkedList:
     def __init__(self):
-        self.head = Node("**dummy**", None)
+        self.head = Node("å**dummy**", None)
 
     def search(self,word):
         current = self.head
@@ -59,19 +59,19 @@ class FreqLinkedList:
             previousNode = None
             bigger = True
             while nextNode != None and bigger:
-                if word < nextNode.getData():
-                    previousNode = nextNode#pizza
-                    nextNode = nextNode.getNext()#ajar
+                if word > nextNode.getData():
+                    previousNode = nextNode
+                    nextNode = nextNode.getNext()
                 else:
                     if previousNode == None:
                         currentNode.setNext(nextNode)
                         self.head = currentNode
                         bigger = False
                     else:
-                        tempnext = nextNode
-                        temp = previousNode
-                        temp.setNext(currentNode)
-                        currentNode.setNext(tempnext)
+                        temprev = previousNode
+                        temp = nextNode
+                        temprev.setNext(currentNode)
+                        currentNode.setNext(temp)
                         bigger = False
 
     def remove(self,word):
@@ -105,86 +105,4 @@ class FreqLinkedList:
             current = current.getNext()
 
 
-d = FreqLinkedList()
 
-d.addWord("ajar")
-d.addWord("pizza")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("bar")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("bar")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("bar")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("bar")
-d.addWord("car")
-d.addWord("car")
-d.addWord("car")
-d.addWord("bar")
-d.addWord("zebra")
-d.addWord("woho")
-d.addWord("zasca")
-d.addWord("pizza")
-d.addWord("pizza")
-
-d.printList()
-
-'''
-    def alphaOrder(self,word):
-        current = [self.head, ""]
-        previous = None
-        comparator = True
-        while current[0] != None or comparator:
-            if current[0] != None:
-                if word < current[0].getData():
-                    if previous == None:
-                        current = [current[0], "prefix"]
-                        comparator = False
-                    else:
-                        current = [previous[0], "infix"]
-                        comparator = False
-                else:
-                    previous = current[0]
-                    current = [current[0].getNext(), '']
-                    comparator = True
-            else:
-                current = [previous, "prefix"]
-                comparator = False
-        print(current)
-        return current
-
-
- def addWord(self, word):
-        if self.search(word):
-            self.head.addFreq()
-        else:
-            if self.size() < 2:
-                self.head = Node(word, self.head)
-            else:
-                nextNode = self.head.getNext()
-                self.head = Node(word, self.head)
-                previousNode = None
-                bigger = True
-                while nextNode != None and not bigger:
-                    if self.head.getData() > nextNode.getData():
-                        previousNode = nextNode
-                        nextNode = nextNode.getNext()
-                    else:
-                        if previousNode == None:
-                            self.head.setNext(nextNode)
-                            bigger = False
-                        else:
-                            self.head.setNext(nextNode)
-                            previous.setNext(self.head)
-                            bigger = False
-
-'''
